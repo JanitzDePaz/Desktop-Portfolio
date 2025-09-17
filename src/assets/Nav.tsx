42ed58e8238af2ce735d3126ea77025cb25ef824
JanitzDePaz <janitzdp@gmail.com>
2025-09-17 11:34:46 +0200

    Incluido hotbar

--- src/assets/Nav.tsx
+++ src/assets/Nav.tsx
@@ -1,7 +1,10 @@
 function Nav(){
     return(
-        <nav className="bg-[#fafafa] fixed bottom-[0] h-[10vh]">
-            <a href="">Link1</a>
+        <nav className="bg-[#404040] fixed bottom-[0] h-[8vh] w-screen flex flex-row">
+            <button className="hotbar__btn"><img src="./public/hotbar/perfil.svg" alt="Perfil" className="hotbar__img"/></button>
+            <button className="hotbar__btn"><img src="./public/hotbar/proyectos.svg" alt="Perfil" className="hotbar__img"/></button>
+            <button className="hotbar__btn"><img src="./public//hotbar/contacto.svg" alt="Perfil" className="hotbar__img"/></button>
+            <button className="hotbar__btn"><img src="./public/hotbar/configuracion.svg" alt="Perfil" className="hotbar__img"/></button>
         </nav>
     )
 }
