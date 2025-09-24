226e4556bd956ff36b27eea1f125ef03b95322b4
JanitzDePaz <janitzdp@gmail.com>
2025-09-24 12:20:05 +0200

    Ventanas con su propio contenido

--- src/components/Ventanas.tsx
+++ src/components/Ventanas.tsx
@@ -1,11 +1,15 @@
 
 import { useEffect, useState } from "react";
 import Perfil from "../data/Perfil";
+import Contacto from "../data/Contacto";
+import Proyectos from "../data/Proyectos";
+import Configuracion from "../data/Configuracion";
 
 type tipoVentana= {
-    tipo: String,
-    cerrar: ()=> void;
+    tipo: string,
+    cerrar: ()=> void,
 }
+
 export default function Ventanas({tipo, cerrar}: tipoVentana){
     const [agarrado, setAgarrando] = useState(false);
     const [posicion, setPosicion] = useState({x:200, y: 200})
@@ -18,7 +22,6 @@ export default function Ventanas({tipo, cerrar}: tipoVentana){
 
     const elementoSoltado= () => {
         setAgarrando(false);
-        console.log("Has soltado el elemento")
     }
 
     const movimiento = (e: React.MouseEvent) => {
@@ -33,20 +36,26 @@ export default function Ventanas({tipo, cerrar}: tipoVentana){
     }, []);
 
     useEffect(() => {
-    const mover = (e: MouseEvent) => {
-        if (agarrado) {
-            setPosicion({ x: e.clientX - compensacion.x, y: e.clientY - compensacion.y });
-        }
-    };
-    window.addEventListener("mousemove", mover);
-    return () => window.removeEventListener("mousemove", mover);
-    }, [agarrado, compensacion]);
+        const mover = (e: MouseEvent) => {
+            if (agarrado) {
+                setPosicion({ x: e.clientX - compensacion.x, y: e.clientY - compensacion.y });
+            }
+        };
+        window.addEventListener("mousemove", mover);
+        return () => window.removeEventListener("mousemove", mover);
+        }, [agarrado, compensacion]);
 
 
+        const contenidos: { [key: string]: React.ReactNode } = {
+        Perfil: <Perfil />,
+        Contacto: <Contacto />,
+        Proyectos: <Proyectos />,
+        Configuracion: <Configuracion />,
+        };
     return (
         <>
-        <div className="flex flex-col bg-[white] w-[100vh] h-[70vh] fixed rounded-[20px]" style={{ left: posicion.x, top: posicion.y }} onMouseMove={movimiento} >
-            <header className="bg-[rgba(50,66,71,1)] h-[6%] rounded-t-[20px] border-b-[4px] border-[black] flex cursor-move select-none" onMouseDown={elementoAgarrado} onMouseUp={elementoSoltado} >
+        <div className="flex flex-col bg-[white] w-[50vw] h-[64vh] fixed rounded-[20px]" style={{ left: posicion.x, top: posicion.y }} onMouseMove={movimiento} >
+            <header className="bg-[rgba(50,66,71,1)] h-[4vh] rounded-t-[20px] border-b-[4px] border-[black] flex cursor-move select-none" onMouseDown={elementoAgarrado} onMouseUp={elementoSoltado} >
                 <section className="w-[20%] flex ">
                     <img src={`/hotbar/${tipo}.svg`} alt="prueba" className="h-[70%] rounded-all place-self-center m-[5%]" />
                 </section>
@@ -58,8 +67,8 @@ export default function Ventanas({tipo, cerrar}: tipoVentana){
                     <button onClick={cerrar} className="w-[15%] place-self-center mr-[5%] bg-transparent border-transparent"><img src="/menuVentanas/btnCerrarVentana.svg" alt="BotonCierre" className="w-[100%]" /></button>
                 </section>
             </header>
-            <main className="bg-[rgba(255,255,255,0.5)] h-[94%] rounded-b-[20px] border-[2px] border-[rgba(133,247,222,1)]">
-                <Perfil />
+            <main className="bg-[rgba(255,255,255,0.5)] h-[60vh] w-[100%] rounded-b-[20px] border-[2px] border-[rgba(133,247,222,1)] overflow-y-auto">
+                {contenidos[tipo]}
             </main>
         </div>
         </>
--- src/data/Perfil.tsx
+++ src/data/Perfil.tsx
@@ -1,26 +1,20 @@
 export default function Perfil(){
     return(
-        <div className="flex mt-[4%] justify-center">
-            <section className="w-[20vh]">
-                <img src="hotbar/Perfil.svg" alt="mi foto" className="w-[100%]" />
-                <img src="" alt="" />
-                <img src="" alt="" />
-            </section>
-            
-            <section className="w-[80vh]">
-                <h1>Janitz De Paz  -  Estudiante de desarrollo web</h1>
-                <div className="flex flex-col">
-                    <h2>¿Quien soy?</h2>
-                    <hr className=" w-[70%]" />
-                    <p className="text-[1.2rem]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, doloribus. Accusantium, quae! Dolores laborum eum et error aperiam! Deserunt aliquam nesciunt consectetur magnam vitae nihil ea beatae laborum. Repellat, ut.</p>
+        <div className="flex w-[50vw]">
+            <section className="flex w-50%">
+                <div>
+                    <img src="" alt="" />
                 </div>
-                <div className="mt-[2%]">
-                    <h2>¿Cual es mi objetivo?</h2>
-                    <hr />
-                    <p className="text-[1.2rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam alias placeat dolorem veniam aut rerum quibusdam, magni facere, odio illo commodi sequi. Consectetur cum perferendis corrupti, quasi provident quidem!</p>
+                <div>
+                    <h1>¿Quien soy?</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero optio vero earum maiores modi adipisci minus distinctio delectus aliquam obcaecati eum, sequi rem recusandae non veritatis, nulla commodi qui aut.</p>
                 </div>
             </section>
-            <section className="flex-1/2"></section>
+            <section>
+                <div>
+                    <h1>Tecnologias</h1>
+                </div>
+            </section>
         </div>
     )
 }
